a151367cc028eef659c7a2ce7e3275093cbc7eb1
Ryusuke-Shibata <major.ryusuke@gmail.com>
2026-06-02 00:11:50 +0900

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,18 +1,64 @@
-// ====== ヘッダー・フッターの読み込み ======
+// ======================
+// 共通パーツ読込
+// ======================
+
 async function includeParts() {
-  const elements = document.querySelectorAll("[data-include]");
+
+  const elements =
+    document.querySelectorAll(
+      "[data-include]"
+    );
 
   for (const el of elements) {
-    const url = el.getAttribute("data-include");
+
+    const path =
+      el.dataset.include;
 
     try {
-      const res = await fetch(url);
-      const html = await res.text();
-      el.outerHTML = html;
-    } catch (err) {
-      console.error("Include error:", url, err);
+
+      const res =
+        await fetch(
+          path,
+          {
+            cache: "no-cache"
+          }
+        );
+
+      if (!res.ok) {
+
+        throw new Error(
+          `${path} の読込失敗`
+        );
+
+      }
+
+      const html =
+        await res.text();
+
+      el.outerHTML =
+        html;
+
     }
+
+    catch(error){
+
+      console.error(error);
+
+      el.innerHTML =
+
+      `
+      <p>
+      読込失敗
+      </p>
+      `;
+
+    }
+
   }
+
 }
 
-document.addEventListener("DOMContentLoaded", includeParts);
+document.addEventListener(
+  "DOMContentLoaded",
+  includeParts
+);
